db9fb26ccfbd4ed0b7c47137b3d854c20c5881ca
hernanbrue <hjbrue@gmail.com>
2020-09-12 12:26:59 -0300

    se agregó la selección de niveles mediante botones

--- assets/js/Scene1.js
+++ assets/js/Scene1.js
@@ -19,6 +19,11 @@ class Scene1 extends Phaser.Scene {
       this.load.tilemapTiledJSON('tilemaps', './assets/json/tilemaps.json'); //agrego el tilemap
       this.load.image('tiles', './assets/json/sprites_arkanoid.png'); //agrego locación en png y le doy una key
 
+      //CARGA BOTONES DE NIVELES (no usé tiles porque me los compilaba en una imágen roja en lugar de lo que eran, 2 botones blancos)
+      this.load.image('nivel1', './assets/nivel1.jpg');
+      this.load.image('nivel2', './assets/nivel2.jpg');
+
+
       //CARGA CORAZÓN
       this.load.image('corazon', './assets/corazon.png');
 
@@ -35,10 +40,19 @@ class Scene1 extends Phaser.Scene {
     create() {
 
       var intro = this.add.image(400, 300, 'intro');
+
+      //agrego los botones en pantalla
+      var nivel1 = this.add.image(200, 400, 'nivel1');
+      var nivel2 = this.add.image(200, 500, 'nivel2');
+
+      nivel1.setInteractive();
+      nivel1.on('pointerdown',() => this.scene.start('escena2'));
+      nivel2.setInteractive();
+      nivel2.on('pointerdown',() => this.scene.start('escena3'));
       
       
-      intro.setInteractive();
-      intro.on('pointerdown',() => this.scene.start('escena2'));
+      /*intro.setInteractive();
+      intro.on('pointerdown',() => this.scene.start('escena2'));*/
       
 
 }
